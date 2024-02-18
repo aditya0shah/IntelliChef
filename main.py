002272b7a8e976796c870bfbf2b0c0cd51acffb3
Siddhant Lodha <57517249+lodhasid@users.noreply.github.com>
2024-02-18 14:55:17 -0800

    camera_test

--- main.py
+++ main.py
@@ -8,5 +8,27 @@ app = Flask(__name__)
 def home_page():
     return render_template("index.html")
 
+@app.route("/camera")
+def camera_view():
+    filename=''     # using filename variable to display video feed and captured image alternatively on the same page
+    image_data_url = request.form.get('image')
+    if request.method == 'POST':
+        # Decode the base64 data URL to obtain the image data
+        image_data = base64.b64decode(image_data_url.split(',')[1])
+        # Create an image from the decoded data
+        img = Image.open(BytesIO(image_data))
+        # Generate a filename with the current date and time
+        timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        filename = f"img_{timestamp}.png"  # Change file extension to 'png'
+        print(filename)
+        # Save the image in PNG format
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        img.save(file_path, 'PNG')
+        error_message = 'Image successfully captured'
+        # use if you want to display all the images in the folder
+        # image_files = os.listdir(app.config['UPLOAD_FOLDER'])
+        return render_template('camera_view.html', filename=filename)
+    return render_template('camera_view.html', filename=filename)
+
 if __name__ == '__main__':
    app.run()
